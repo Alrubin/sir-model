07dc53ad5c36a187c51b9951313c084e70046733
Alessandro Rubin <alex.rubin@outlook.com>
2024-04-20 19:24:45 +0200

    Refactor: introduced validation with pydantic

--- lib/scenario.py
+++ lib/scenario.py
@@ -1,14 +1,5 @@
-class InvalidPopulationError(ValueError):
-    pass
+from pydantic import BaseModel, NonNegativeInt
 
 
-class Scenario:
-    def __init__(self, population):
-        self.validate_population_attribute(population)
-        self.population = population
-
-    def validate_population_attribute(self, population):
-        if not isinstance(population, int):
-            raise InvalidPopulationError("The population parameter must be an integer")
-        if population < 0:
-            raise InvalidPopulationError("The population parameter must be greater or equal than 0")
+class Scenario(BaseModel):
+    population: NonNegativeInt
--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from parameterized import parameterized
-from lib.scenario import Scenario, InvalidPopulationError
+from lib.scenario import Scenario
+from pydantic import ValidationError
 
 
 class ScenarioTestCase(TestCase):
@@ -12,23 +13,22 @@ class ScenarioTestCase(TestCase):
     ])
     def test_raise_error_on_negative_population(self, population, expected_to_raise_exception):
         if expected_to_raise_exception:
-            with self.assertRaises(InvalidPopulationError) as context:
-                Scenario(population)
-
-            self.assertEquals("The population parameter must be greater or equal than 0", str(context.exception))
+            with self.assertRaises(ValidationError):
+                Scenario(population=population)
         else:
-            Scenario(population)
+            Scenario(population=population)
 
     @parameterized.expand([
         (2, False),
-        (2.0, True),
-        (2.1, True)
+        (2.0, False),
+        (2.1, True),
+        ("2", False),
+        ("2.1", True),
+        (None, True)
     ])
     def test_raise_when_population_is_not_an_integer(self, population, expected_to_raise_exception):
         if expected_to_raise_exception:
-            with self.assertRaises(InvalidPopulationError) as context:
-                Scenario(population)
-
-            self.assertEquals("The population parameter must be an integer", str(context.exception))
+            with self.assertRaises(ValidationError):
+                Scenario(population=population)
         else:
-            Scenario(population)
+            Scenario(population=population)
