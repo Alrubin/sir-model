29212fb1db03820b61160ef3d89e5f68219532b0
Alessandro Rubin <alex.rubin@outlook.com>
2024-04-28 15:19:17 +0200

    rename

--- tests/test_disease.py
+++ tests/test_disease.py
@@ -8,7 +8,7 @@ from lib.disease import Disease
 class DiseaseTest(TestCase):
 
     def test_base(self):
-        disease = my_disease(0.1, 0.1)
+        disease = disease_builder(0.1, 0.1)
 
         self.assertCountEqual(["transmission_rate","recovery_rate"], disease.rates)
     @parameterized.expand([
@@ -17,13 +17,13 @@ class DiseaseTest(TestCase):
         (1,1)
     ])
     def test_on_mean_duration_of_infection(self, recovery_rate, expected_mean_duration):
-        disease = my_disease(0.1, recovery_rate)
+        disease = disease_builder(0.1, recovery_rate)
 
         mean_duration = disease.mean_duration()
 
         self.assertEqual(expected_mean_duration, mean_duration)
 
-def my_disease(transmission_rate, recovery_rate):
+def disease_builder(transmission_rate, recovery_rate):
     return Disease(transmission_rate=transmission_rate,
                    recovery_rate=recovery_rate
                    )
