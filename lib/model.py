461ca086563ce9c786166ea4144ac07895c857ee
Alessandro Rubin <alex.rubin@outlook.com>
2024-04-28 15:18:57 +0200

    Definita classe SIRModel

--- lib/model.py
+++ lib/model.py
@@ -1,6 +1,7 @@
 from typing import List
-
 from pydantic import BaseModel, NonNegativeInt
+from lib.disease import Disease
+
 
 class SIRModelPopulation(BaseModel):
     susceptible: NonNegativeInt
@@ -11,3 +12,15 @@ class SIRModelPopulation(BaseModel):
     def states(self) -> List[str]:
         return list(self.__dict__.keys())
 
+
+class SIRModel:
+    def __init__(self, disease: Disease):
+        self.tr = disease.transmission_rate
+        self.rr = disease.recovery_rate
+
+    def __call__(self, y: SIRModelPopulation):
+        S, I, R = y.susceptible, y.infected, y.removed
+        dSdt = -self.tr * S * I
+        dIdt = self.tr * S * I - self.rr * I
+        dRdt = self.rr * I
+        return dSdt, dIdt, dRdt
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
 import unittest
-
+from lib.disease import Disease
 from lib.model import SIRModelPopulation
+from lib.model import SIRModel
 
 
 class SIRModelPopulationTest(unittest.TestCase):
@@ -14,4 +15,12 @@ class SIRModelPopulationTest(unittest.TestCase):
         self.assertEqual(15, distribution.removed)
 
 
+class SIRModelTest(unittest.TestCase):
+    def test_base(self):
+        initial_conditions = SIRModelPopulation(susceptible=10, infected=10, removed=10)
+        disease = Disease(transmission_rate=0.1, recovery_rate=0.1)
+        model = SIRModel(disease=disease)
 
+        gradient = model(initial_conditions)
+
+        self.assertEqual((-10.0, 9.0, 1.0), gradient)
