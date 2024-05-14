b2f0832f34b0b527659cc997dc814e5aa4a7c321
Alessandro Rubin <alex.rubin@outlook.com>
2024-05-14 22:06:13 +0200

    Refactor: spostata classe di test

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,23 +4,6 @@ from lib.population import build_sir_population
 from lib.model import SIRModel
 
 
-class PopulationStateTest(unittest.TestCase):
-
-    def test_base(self):
-        population = build_sir_population(5, 10, 15)
-
-        self.assertEqual(5, population.susceptibles.value)
-        self.assertEqual(10, population.infected.value)
-        self.assertEqual(15, population.removed.value)
-
-    def test_conversion_as_array(self):
-        population = build_sir_population(5, 10, 15)
-
-        population_array = population.array()
-
-        self.assertEqual([5, 10, 15], population_array)
-
-
 class SIRModelTest(unittest.TestCase):
     def test_base(self):
         initial_conditions = build_sir_population(10, 10, 10)
