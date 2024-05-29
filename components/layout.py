1daf7069f5274bd23ae14a4754305cc845831ac4
Alessandro Rubin <alex.rubin@outlook.com>
2024-05-29 19:22:18 +0200

    Creation of a left sidebar

--- components/layout.py
+++ components/layout.py
@@ -5,8 +5,24 @@ from components.navbar import Navbar
 class AppLayout:
     def __call__(self):
         return html.Div([
-            self.navbar()
-            ])
+            self.navbar(),
+            self.left_sidebar(),
+        ])
 
     def navbar(self):
         return Navbar()()
+
+    def left_sidebar(self):
+        return html.Div(children=[], style={
+            "position": "fixed",
+            "top": 62.5,
+            "left": 0,
+            "bottom": 0,
+            "width": "24rem",
+            "height": "100%",
+            "z-index": 1,
+            "overflow-x": "hidden",
+            "transition": "all 0.5s",
+            "padding": "2rem 1rem",
+            "background-color": "#f8f9fa",
+        })
