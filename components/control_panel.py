ff706056a1aae6201f0854ca22f7e9714dc6036f
Alessandro Rubin <alex.rubin@outlook.com>
2024-05-31 22:45:38 +0200

    Set a minim value for Inputs

--- components/control_panel.py
+++ components/control_panel.py
@@ -11,16 +11,15 @@ class ControlPanel:
 
         return html.Div(children=accordion)
 
-
     def initial_conditions_panel(self):
         return dbc.Col(children=[
-            self.initial_condition(field="Susceptibles", input_id="S0",initial_value=100),
+            self.initial_condition(field="Susceptibles", input_id="S0", initial_value=100),
             self.initial_condition(field="Infected", input_id="I0", initial_value=0),
             self.initial_condition(field="Removed", input_id="R0", initial_value=0),
         ])
 
     def initial_condition(self, field: str, input_id: str, initial_value: int):
         return dbc.Row(children=[
-                    dbc.Label(children=f"{field}:", width=7),
-                    dbc.Col(dbc.Input(id=input_id, type="number", value=initial_value, size="sm"), width=5)
-                    ], class_name="mb-3")
+            dbc.Label(children=f"{field}:", width=7),
+            dbc.Col(dbc.Input(id=input_id, type="number", value=initial_value, size="sm", min=0, step=1), width=5)
+        ], class_name="mb-3")
