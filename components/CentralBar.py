3fb5b5d1ee60df207415619510d4daba580bc7bf
Alessandro Rubin <alex.rubin@outlook.com>
2024-09-05 23:15:07 +0200

    Stile di CentralBar spostato in un file css

--- components/CentralBar.py
+++ components/CentralBar.py
@@ -2,12 +2,6 @@ from dash.html import Div
 
 
 class CentralBar(Div):
-    layout = {
-        "margin-left": "25rem",
-        "margin-right": "0rem",
-        "padding": "2rem 1rem",
-    }
-
     def __init__(self, children: list):
         self.children = children
-        super().__init__(children=self.children, style=self.layout)
+        super().__init__(children=self.children, className="central-bar")
