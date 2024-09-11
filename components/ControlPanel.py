1e42fccde720cf316b1045d9cbc2f17983065b1c
Alessandro Rubin <alex.rubin@outlook.com>
2024-09-11 22:40:23 +0200

    Il pannello di controllo calcola i suscettibili iniziali

--- components/ControlPanel.py
+++ components/ControlPanel.py
@@ -1,10 +1,20 @@
-from dash_bootstrap_components import Row, Label, Col, Accordion, AccordionItem, Input
+import dash
+from dash_bootstrap_components import Row, Label, Col, Accordion, AccordionItem, Input, Button
 from dash.html import Div
 
 
 class ControlPanel(Div):
 
     def __init__(self):
+        @dash.callback(
+            dash.Output("S0", "children"),
+            [dash.Input(component_id='N0', component_property='value'),
+             dash.Input(component_id='I0', component_property='value'),
+             dash.Input(component_id='R0', component_property='value')
+             ])
+        def initial_susceptibles(N, I0, R0):
+            return f"Suscettibili: {N - I0 - R0}"
+
         accordion = Accordion(children=[
             AccordionItem(children=[self.initial_conditions_panel()], title="Condizioni demografiche iniziali"),
             AccordionItem(children=[self.disease_properties_panel()], title="Caratteristiche del patogeno")
@@ -17,6 +27,7 @@ class ControlPanel(Div):
             self.initial_condition(field="Popolazione", input_id="N0", initial_value=90000, min=0, step=1),
             self.initial_condition(field="Infetti", input_id="I0", initial_value=0, min=0, step=1),
             self.initial_condition(field="Rimossi", input_id="R0", initial_value=0, min=0, step=1),
+            Col(Button(id="S0", color="light", class_name="d-grid gap-2 col-12 mx-auto")),
         ])
 
     def disease_properties_panel(self):
