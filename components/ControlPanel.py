048aa3b13564965e426f196048e5906ad57bf0fd
Alessandro Rubin <alex.rubin@outlook.com>
2024-09-19 21:52:51 +0200

    Introdotto pulsante per refreshare la pagina (e resettare i valori)

--- components/ControlPanel.py
+++ components/ControlPanel.py
@@ -1,6 +1,8 @@
 import dash
-from dash_bootstrap_components import Row, Label, Col, Accordion, AccordionItem, Input, Button
-from dash.html import Div
+from dash.dcc import Location
+from dash_bootstrap_components import Row, Label, Col, Accordion, AccordionItem, Input, Button, CardBody, Card, \
+    CardHeader
+from dash.html import Div, P, A
 
 from lib.population import InitialValues
 
@@ -18,12 +20,35 @@ class ControlPanel(Div):
             initial_values = InitialValues(population=N, infected=I0, removed=R0)
             return f"Suscettibili: {initial_values.susceptibles}"
 
-        accordion = Accordion(children=[
+        @dash.callback(
+            dash.Output('current-url', 'href'),
+            dash.Input('reset-button', 'n_clicks')
+        )
+        def refresh_page(n_clicks):
+            if n_clicks:
+                return '/'
+            return None
+
+        card_header = CardHeader("Pannello di controllo")
+        card_body = CardBody(children=[
+            self.settings(),
+            P(),
+            self.reset_button(),
+            Location(id='current-url', refresh=True)
+        ])
+
+        super().__init__(children=Card([card_header, card_body]))
+
+    def settings(self):
+        return Accordion(children=[
             AccordionItem(children=[self.initial_conditions_panel()], title="Condizioni demografiche iniziali"),
             AccordionItem(children=[self.disease_properties_panel()], title="Caratteristiche del patogeno")
         ], start_collapsed=True)
 
-        super().__init__(children=accordion)
+    def reset_button(self):
+        return Row([
+            Button("Resetta valori", color="warning", class_name="me-1", id="reset-button")
+        ], class_name="mb-3", style={"width": "20rem", "margin": "auto"})
 
     def initial_conditions_panel(self):
         return Col(children=[
